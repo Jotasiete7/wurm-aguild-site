75967274ea299e4788ada247d05de4234a95b9c0
Jota7 <jaimeengelmann@gmail.com>
2026-05-01 13:15:41 -0300

    feat: wrap App component with LayoutBase in main entry point

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
-import { LayoutBase } from '@antigravity/layout/LayoutBase'
+import { LayoutBase } from '@ecossistema-guilda/layout/LayoutBase'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
